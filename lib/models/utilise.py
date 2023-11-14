791d910925aba333a11369a6027cf6670b343924
He Lirong <53851874+LR-He@users.noreply.github.com>
2023-11-14 15:12:53 +0800

    Update utilise.py

--- lib/models/utilise.py
+++ lib/models/utilise.py
@@ -355,7 +355,7 @@ class TemporallySharedBlock(nn.Module):
     def __init__(self, pad_value: Optional[float] = None):
         super().__init__()
         # self.out_shape = None
-        self.out_shape = [None, None, None, None]
+        self.out_shape = [0, 0, 0, 0]
         self.pad_value = pad_value
 
     def smart_forward(self, x: Tensor, pad_mask: Optional[Tensor] = None) -> Tensor:
